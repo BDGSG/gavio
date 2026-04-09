78909f31c5e397931967703cfd56d5f1123bbb1f
BDGSG <BDGSG@users.noreply.github.com>
2026-04-09 20:45:11 +0200

    Fix Stripe: remove explicit apiVersion to avoid TS error

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,5 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-09-30.acacia',
-  typescript: true,
-})
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET!
